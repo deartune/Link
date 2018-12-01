c74280a9483d95ea78279d38e032f03df0f595d0
stoner <deartune@gmail.com>
2018-12-02 06:01:54 +0900

    recycler & db link added    //but needs SimpleCursorAdapter?

--- app/src/main/java/com/womenproiot/www/link/AttendeesActivity.java
+++ app/src/main/java/com/womenproiot/www/link/AttendeesActivity.java
@@ -24,7 +24,8 @@ public class AttendeesActivity extends AppCompatActivity implements AdapterView.
     private DbHelper dbHelper;
     private SQLiteDatabase mdb;
     ImageButton buttonNew,buttonCenter;
-    String seletedMeetUp;
+    String getSeq,Items;
+    Spinner spinner;
     ArrayAdapter dataAdapter1;
     Cursor c;
     @Override
@@ -43,14 +44,15 @@ public class AttendeesActivity extends AppCompatActivity implements AdapterView.
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
 
 
-
-        String query = "SELECT title FROM meetup ";
+        String query = "SELECT seq,title FROM meetup order by reg_date asc";
         c = mdb.rawQuery(query,null);
 
         ArrayList<String> list=new ArrayList<String>();
         while(c.moveToNext()){
-            String getTitle=c.getString(0);
+            String getTitle=c.getString(1);
+            getSeq=c.getString(0);
             list.add(getTitle);
+
         }
 
         dataAdapter1 =
@@ -74,20 +76,34 @@ public class AttendeesActivity extends AppCompatActivity implements AdapterView.
     }
 
     private List<String> createItemList() {
-        List<String> items = new ArrayList<>();
-        for (int i = 0; i < 20; i++) {
-            items.add("아이템 " + i);
+
+
+
+        String query1 = "SELECT address FROM attendee where fr_seq='"+getSeq+"'";
+        c = mdb.rawQuery(query1,null);
+
+        ArrayList<String> items=new ArrayList<String>();
+        while(c.moveToNext()){
+            String getAddress=c.getString(0);
+            items.add(getAddress);
         }
+
         return items;
+
+
     }
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
 
-            seletedMeetUp = (String)dataAdapter1.getItem(position);
+        String query1 = "SELECT address FROM attendee where fr_seq='"+getSeq+"'";
+        c = mdb.rawQuery(query1,null);
 
-
-
+        ArrayList<String> items=new ArrayList<String>();
+        while(c.moveToNext()){
+            String getAddress=c.getString(0);
+            items.add(getAddress);
+        }
 
     }
 
@@ -101,10 +117,10 @@ public class AttendeesActivity extends AppCompatActivity implements AdapterView.
 
     @Override
     public void onClick(View v) {
-      // String query ="INSERT INTO attendee (seq,name,latitude,longitude,reg_date,modi_date,fr_code,address) values ('m0173','ㅎㅎㅎ광화문역 5호선', 37.5712497, 126.9773945, '2018/11/29 06:20:09',null,'QuQTW2cBOAUY6uUl2HEK','서울특별시 종로구 세종대로 172' )";
+     //  String query ="INSERT INTO attendee (fr_seq,name,latitude,longitude,reg_date,modi_date,fr_code,address) values ('m0179','ㅎㅎㅎ광화문역 5호선', 37.5712497, 126.9773945, '2018/11/29 06:20:09',null,'QuQTW2cBOAUY6uUl2HEK','과천시 종로구 세종대로 172' )";
         //"INSERT INTO MEETUP VALUES ('" + str + "','" + title + "','" + seletedAge + "','" + seletedGender + "','" + formatDate + "','" + null + "')";
-       // String query="INSERT INTO MEETUP VALUES ('m0173','목요일엔돼지모임', '20대', '남성','2018/11/29 06:20:09',null);";
-      //  mdb.execSQL(query);
-       // Toast.makeText(getApplicationContext(),"attendee777 등록 완료", LENGTH_SHORT).show();
+      // String query="INSERT INTO MEETUP VALUES ('m0179','수요일엔독서모임', '40대', '여성','2018/11/29 06:20:09',null);";
+      //   mdb.execSQL(query);
+    //   Toast.makeText(getApplicationContext(),"완료", LENGTH_SHORT).show();
     }
 }
